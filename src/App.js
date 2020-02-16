c5ebcb8d38575b49bbac5a2da75b5dc28c2a1b27
Gon Gis <gon.eu.1987@gmail.com>
2020-02-16 11:50:28 -0300

    adding initial components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import SearchBarComponent from "./components/SearchBarComponent";
+import Library from "./components/Library";
 
-function MyReadApp() {
-  return (
-    <div className="App">
+class MyReadApp extends Component{
+  state = {
+    /**
+     * TODO: Instead of using this state variable to keep track of which page
+     * we're on, use the URL in the browser's address bar. This will ensure that
+     * users can use the browser's back and forward buttons to navigate between
+     * pages, as well as provide a good URL they can bookmark and share.
+     */
+    showSearchPage: false
+  }
 
-    </div>
-  );
+  render(){
+    return (
+        <div className="App">
+          {this.state.showSearchPage ? (
+              <SearchBarComponent/>
+          ) : (
+              <Library/>
+          )
+          }
+        </div>
+    );
+  }
+
 }
 
 export default MyReadApp;
